575c30af1ab83b3798b176dbd4eee8115883614a
ykallan <45066189+ykallan@users.noreply.github.com>
2020-08-21 21:53:43 +0800

    Update settings.py

--- bbzbw/settings.py
+++ bbzbw/settings.py
@@ -45,7 +45,7 @@ DEFAULT_REQUEST_HEADERS = {
   'Referer': 'https://www.bibenet.com/database/toOwnerListPage.htm?pageNum=4&pageSize=&type=1&type=1&type=1',
   'Connection': 'keep-alive',
   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36',
-  'Cookie': 'Hm_lvt_72debff28530b8444ed66fb29b59be17=1597999111; UM_distinctid=174102b5d02ae3-0cd97b99da3b69-3323766-1fa400-174102b5d03ad9; Qs_lvt_272444=1597999111; index_layerAdv=113.44.149.232; uuidUV=130bb00ed7dd492ab196f800287a8339; historyUV=2020-08-21T16%3A38%3A33.043%2B08%3A00; mediav=%7B%22eid%22%3A%22570152%22%2C%22ep%22%3A%22%22%2C%22vid%22%3A%22%22%2C%22ctn%22%3A%22%22%2C%22vvid%22%3A%22%22%2C%22_mvnf%22%3A1%7D; nb-referrer-hostname=www.bibenet.com; JSESSIONID=9EC3B869405D5B220E3BAB72B9E693DF; Qs_pv_272444=3883204168876829700%2C1253655791537503500%2C1578746528207361300%2C1989866125495969500%2C1731225993222481400; nb-start-page-url=https%3A%2F%2Fwww.bibenet.com%2Fdatabase%2FtoOwnerListPage.htm%26pageNum%3D1241%26pageSize%3D%26type%3D1%26%3BJSESSIONID%3D3D3F41422862F4340E57D87C5EA499E6%3Fticket%3DST-43039-wqxXbS0xEoPPrNe7nT4u-cas01.example.org; CNZZDATA1261799122=526672187-1597993723-%7C1598016232; Hm_lpvt_72debff28530b8444ed66fb29b59be17=1598016303',
+  'Cookie': '加上你自己的cookies就好了',
 }
 
 # Enable or disable spider middlewares
